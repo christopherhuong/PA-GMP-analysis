7e7e93bab74d727fc20893aaee02f44c0a01058a
chrishuong95@gmail.com <chrishuong95@gmail.com>
2022-07-21 19:10:54 -0700

    asdas

--- mhm.R
+++ mhm.R
@@ -1,1 +1,21 @@
-dat <- read.csv()
+library(readr)
+library(curl)
+library(tidyverse)
+library(dplyr)
+library(psych)
+library(corrplot)
+library(ggplot2)
+library(car)
+
+dat1 <- read.csv("mhm_data_2022-07-21_19-46-52.csv", 
+                 stringsAsFactors = T)
+
+
+dat2 <- select(dat1, Adaptability.to.Change:Adult.traumas)
+
+dat2 <- dat2[,-which(sapply(dat2, class) == "factor")]
+
+cormax <- cor(dat2, use = "pairwise.complete.obs")
+KMO(dat2)
+cortest.bartlett(dat2)
+det(cormax)
