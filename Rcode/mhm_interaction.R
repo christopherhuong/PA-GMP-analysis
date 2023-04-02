c4a81bd043b472a97cec9609e830ee9c95732c9f
chrishuong95@gmail.com <chrishuong95@gmail.com>
2023-04-01 20:58:25 -0500

    asda

---
+++ Rcode/mhm_interaction.R
@@ -0,0 +1,489 @@
+library(tidyverse)
+library(survey)
+library(knitr)
+library(ggplot2)
+
+
+load('mhm.rdata')
+
+
+load('gbm_interact1.rdata')
+load('gbm_interact2.rdata')
+load('gbm_interact3.rdata')
+load('gbm_interact4.rdata')
+load('gbm_interact5.rdata')
+load('gbm_interact6.rdata')
+load('gbm_interact7.rdata')
+load('gbm_interact8.rdata')
+
+
+
+
+
+interact <- function(a,b) {
+  des_int <- svydesign(ids = ~country, weights = a$weights,
+                       data = subset(mhm, age==b))
+  
+  mhq_interact <- svyglm(mhq ~ PA,
+                         design = des_int,
+                         family = gaussian())
+  
+}
+
+
+
+a<-interact(gbm_interact1, 1)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact2, 2)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact3, 3)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact4, 4)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact5, 5)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact6, 6)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact7, 7)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact8, 8)
+a$coefficients
+confint(a)
+
+
+
+
+
+
+
+
+
+# cog ---------------------------------------------------------------------
+
+
+interact <- function(a,b) {
+  des_int <- svydesign(ids = ~country, weights = a$weights,
+                       data = subset(mhm, age==b))
+  
+  mhq_interact <- svyglm(cog ~ PA,
+                         design = des_int,
+                         family = gaussian())
+  
+  
+}
+
+
+
+a<-interact(gbm_interact1, 1)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact2, 2)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact3, 3)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact4, 4)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact5, 5)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact6, 6)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact7, 7)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact8, 8)
+a$coefficients
+confint(a)
+
+
+
+# adaptresil --------------------------------------------------------------
+
+
+interact <- function(a,b) {
+  des_int <- svydesign(ids = ~country, weights = a$weights,
+                       data = subset(mhm, age==b))
+  
+  mhq_interact <- svyglm(adaptresil ~ PA,
+                         design = des_int,
+                         family = gaussian())
+  
+  
+}
+
+
+a<-interact(gbm_interact1, 1)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact2, 2)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact3, 3)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact4, 4)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact5, 5)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact6, 6)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact7, 7)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact8, 8)
+a$coefficients
+confint(a)
+
+
+
+
+# moodoutlook -------------------------------------------------------------
+
+
+
+interact <- function(a,b) {
+  des_int <- svydesign(ids = ~country, weights = a$weights,
+                       data = subset(mhm, age==b))
+  
+  mhq_interact <- svyglm(moodoutlook ~ PA,
+                         design = des_int,
+                         family = gaussian())
+  
+
+}
+
+
+
+
+a<-interact(gbm_interact1, 1)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact2, 2)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact3, 3)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact4, 4)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact5, 5)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact6, 6)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact7, 7)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact8, 8)
+a$coefficients
+confint(a)
+
+
+
+# drive motivation --------------------------------------------------------
+
+interact <- function(a,b) {
+  des_int <- svydesign(ids = ~country, weights = a$weights,
+                       data = subset(mhm, age==b))
+  
+  mhq_interact <- svyglm(drivemotiv ~ PA,
+                         design = des_int,
+                         family = gaussian())
+  
+  
+}
+
+
+
+
+a<-interact(gbm_interact1, 1)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact2, 2)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact3, 3)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact4, 4)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact5, 5)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact6, 6)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact7, 7)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact8, 8)
+a$coefficients
+confint(a)
+
+
+
+
+# social self -------------------------------------------------------------
+
+
+interact <- function(a,b) {
+  des_int <- svydesign(ids = ~country, weights = a$weights,
+                       data = subset(mhm, age==b))
+  
+  mhq_interact <- svyglm(socialself ~ PA,
+                         design = des_int,
+                         family = gaussian())
+  
+
+}
+
+
+
+a<-interact(gbm_interact1, 1)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact2, 2)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact3, 3)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact4, 4)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact5, 5)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact6, 6)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact7, 7)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact8, 8)
+a$coefficients
+confint(a)
+
+
+# mindbody ----------------------------------------------------------------
+
+
+
+interact <- function(a,b) {
+  des_int <- svydesign(ids = ~country, weights = a$weights,
+                       data = subset(mhm, age==b))
+  
+  mhq_interact <- svyglm(mindbody ~ PA,
+                         design = des_int,
+                         family = gaussian())
+  
+  
+}
+
+
+
+a<-interact(gbm_interact1, 1)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact2, 2)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact3, 3)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact4, 4)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact5, 5)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact6, 6)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact7, 7)
+a$coefficients
+confint(a)
+a<-interact(gbm_interact8, 8)
+a$coefficients
+confint(a)
+
+
+
+# plots -------------------------------------------------------------------
+
+library(ggplot2)
+
+mhq <- data.frame(age = c('18-24','25-34','35-44','45-54','55-64','65-74','75-84','85+'),
+                  ATC = c(17.77,	19.79,	20.41,	18.65,	15.76,	12.37,	11.24,	23.37),
+                  low = c(15.55,	17.13,	17.53,	15.15,	11.96,	9.65,	7.89,	12.47),
+                  up = c(19.99,	22.45,	23.29,	22.15,	19.56,	15.10,	14.59,	34.28))
+
+
+ggplot(mhq, aes(x=age, y=ATC, group=1)) + 
+  geom_errorbar(aes(ymin=low, ymax=up), width=.2) +
+  geom_line() + geom_point() + theme_minimal() +
+  labs(title = "Effect of PA on MHQ with 95% CI's", y = "ATC", x = "Age")
+
+
+a <- c('18-24','25-34','35-44','45-54','55-64','65-74','75-84','85+')
+b <- c(17.29,	18.56,	18.79,	16.66,	13.78,	9.74,	  7.84,	  19.25,
+       20.1,	18.71,	18.96,	16.87,	14.79,	14.5, 	12.18,	21.93,
+       13.71,	16.64,	17.94,	16.81,	13.9, 	10.74,	9.98,  	19.4,
+       18.27,	18.34,	17.94,	15.65,	11.71,	9.88,	  8.29,	  20.14,
+       10.96,	14.33,	15.42,	14.84,	12.45,	9.04,	  8.02,	  19.22,
+       19.54,	21.38,	21.12,	19.46,	16.67,	15.38,	15.36,	25.52)
+
+c <- c(15.48,	16.36,	16.18,	13.80,	10.92,	7.33,	4.56,	10.71,
+       17.41,	15.58,	15.80,	13.34,	11.05,	11.34,	8.70,  	14.04,
+       11.62,	14.05,	15.09,	13.28,	9.88,	  8.05,	  7.10,	  9.27,
+       15.88,	15.44,	14.67,	11.97,	7.91,	  6.71,	  4.60,	  12.04,
+       8.43,	11.39,	12.49,	11.36,	8.82,	  6.29,  	4.54,	  7.85,
+       17.50,	18.74,	18.59,	15.79,	12.39,	11.84,	10.91,	16.83)
+
+d <- c(19.09,	20.76,	21.40,	19.52,	16.63,	12.14,	11.12,	27.78,
+       22.80,	21.85,	22.12,	20.39,	18.53,	17.66,	15.65,	29.82,
+       15.80,	19.23,	20.78,	20.35,	17.90,	13.42,	12.86,	29.53,
+       20.66,	21.23,	21.22,	19.33,	15.51,	13.04,	11.98,	28.23,
+       13.50,	17.26,	18.35,	18.32,	16.09,	11.79,	11.49,	30.59,
+       21.58,	24.01,	23.66,	23.13,	20.95,	18.91,	19.80,	34.22)
+
+e <- c("Cog", "Cog", "Cog", "Cog", "Cog", "Cog", "Cog", "Cog", 
+       "Adapt/Resil", "Adapt/Resil", "Adapt/Resil", "Adapt/Resil", "Adapt/Resil", "Adapt/Resil", "Adapt/Resil", "Adapt/Resil", 
+       "Mood/Outlook", "Mood/Outlook", "Mood/Outlook", "Mood/Outlook", "Mood/Outlook", "Mood/Outlook", "Mood/Outlook", "Mood/Outlook", 
+       "Drive/Motivation", "Drive/Motivation", "Drive/Motivation", "Drive/Motivation", "Drive/Motivation", "Drive/Motivation", "Drive/Motivation", "Drive/Motivation", 
+       "Social Self", "Social Self", "Social Self", "Social Self", "Social Self", "Social Self", "Social Self", "Social Self", 
+       "Mind Body", "Mind Body", "Mind Body", "Mind Body", "Mind Body", "Mind Body", "Mind Body", "Mind Body")
+
+
+
+subdomains <- data.frame(age = c(a,a,a,a,a,a),
+                         ATC = b,
+                         low = c,
+                         up = d,
+                         grp = e)
+
+
+ggplot(subdomains, aes(x=age, y=ATC, group=grp, color = grp)) + 
+  geom_errorbar(aes(ymin=low, ymax=up), width=.5, position=position_dodge(0.1)) +
+  geom_line() + geom_point() + theme_minimal() +
+  labs(title = "Effect of PA on Subdomains", y = "ATC", x = "Age")
+
+
+
+# nope 6 plots -------------------------------------------------------------
+
+
+
+# cog ---------------------------------------------------------------------
+
+
+ggplot(subset(subdomains, grp == "Cog"), aes(x=age, y=ATC, group = 1)) + 
+  geom_errorbar(aes(ymin=low, ymax=up), width=.5, position=position_dodge(0.1)) +
+  geom_line() + geom_point() + theme_minimal() +
+  labs(title = "Core Cognition", y = "ATC", x = "Age") +
+  ylim(0 , 35)
+
+
+
+# adapt/resil -------------------------------------------------------------
+
+ggplot(subset(subdomains, grp == "Adapt/Resil"), aes(x=age, y=ATC, group = 1)) + 
+  geom_errorbar(aes(ymin=low, ymax=up), width=.5, position=position_dodge(0.1)) +
+  geom_line() + geom_point() + theme_minimal() +
+  labs(title = "Adaptability and Resilience", y = "ATC", x = "Age") +
+  ylim(0, 35)
+
+
+# mood/outlook ------------------------------------------------------------
+
+ggplot(subset(subdomains, grp == "Mood/Outlook"), aes(x=age, y=ATC, group = 1)) + 
+  geom_errorbar(aes(ymin=low, ymax=up), width=.5, position=position_dodge(0.1)) +
+  geom_line() + geom_point() + theme_minimal() +
+  labs(title = "Mood and Outlook", y = "ATC", x = "Age") +
+  ylim(0, 35)
+
+
+# Drive and Motivation ----------------------------------------------------
+
+ggplot(subset(subdomains, grp == "Drive/Motivation"), aes(x=age, y=ATC, group = 1)) + 
+  geom_errorbar(aes(ymin=low, ymax=up), width=.5, position=position_dodge(0.1)) +
+  geom_line() + geom_point() + theme_minimal() +
+  labs(title = "Drive and Motivation", y = "ATC", x = "Age") +
+  ylim(0, 35)
+
+
+
+# social self -------------------------------------------------------------
+
+ggplot(subset(subdomains, grp == "Social Self"), aes(x=age, y=ATC, group = 1)) + 
+  geom_errorbar(aes(ymin=low, ymax=up), width=.5, position=position_dodge(0.1)) +
+  geom_line() + geom_point() + theme_minimal() +
+  labs(title = "Social Self", y = "ATC", x = "Age") +
+  ylim(0, 35)
+
+
+# mind body ---------------------------------------------------------------
+
+ggplot(subset(subdomains, grp == "Mind Body"), aes(x=age, y=ATC, group = 1)) + 
+  geom_errorbar(aes(ymin=low, ymax=up), width=.5, position=position_dodge(0.1)) +
+  geom_line() + geom_point() + theme_minimal() +
+  labs(title = "Mind Body", y = "ATC", x = "Age") +
+  ylim(0, 35)
+
+
+
+################################################
+
+# average PA --------------------------------------------------------------
+
+
+load('mhm.rdata')
+
+mhm <- mhm %>%
+  mutate(age1 = case_when(age == 1 ~ "18-24",
+                   age == 2 ~ "25-34",
+                   age == 3 ~ "35-44",
+                   age == 4 ~ "45-54",
+                   age == 5 ~ "55-64",
+                   age == 6 ~ "65-74",
+                   age == 7 ~ "75-84",
+                   age == 8 ~ "85+"))
+mhm$age1 <- as.factor(mhm$age1)
+mhm$PA <- factor(mhm$PA,
+                 labels = c("Inactive", "Active"))
+
+
+ggplot(mhm, aes(x=age1, y=mhq, group = PA)) + 
+  geom_errorbar(aes(ymin=low, ymax=up), width=.5, position=position_dodge(0.1)) +
+  geom_line() + geom_point() + theme_minimal() +
+  labs(title = "Mind Body", y = "ATC", x = "Age") +
+  ylim(0, 35)
+
+
+
+
+
+# age main effects --------------------------------------------------------
+
+load('mhm.rdata')
+
+age_lm <- 
+
+
--- Rcode/mhm_secondary_and_sensitivity.R
+++ Rcode/mhm_secondary_and_sensitivity.R
@@ -236,173 +236,173 @@ save(gbm_interact8, file = "gbm_interact8.RData")
 # MHQ INTERACTION ---------------------------------------------------------
 
 
-
-
-des_int <- svydesign(ids = ~country, weights = gbm_interact$weights,
-                 data = mhm)
-
-
-
-mhq_interact <- svyglm(mhq ~ PA*age,
-                      design = des_int,
-                      family = gaussian())
-
-
-summary(mhq_interact)
-# Coefficients:
-#              Estimate Std. Error t value Pr(>|t|)    
-# (Intercept)    4.3888     3.7979   1.156 0.249093    
-#   PA          25.6385     2.6505   9.673  < 2e-16 ***
-#   age          14.9358     0.7656  19.509  < 2e-16 ***
-#   PA:age       -2.0408     0.5796  -3.521 0.000522 *** 
-confint(mhq_interact)
-#                    2.5 %     97.5 %
-#   (Intercept)   -3.095626 11.8731520
-# PA              20.415341 30.8617081
-# age             13.427074 16.4445412
-# PA:age          -3.183107 -0.8985421
-
-library(interactions)
-interactions::interact_plot(mhq_interact,
-                            pred = age, modx = PA)
-# shown in pdf
-
+# 
+# 
+# des_int <- svydesign(ids = ~country, weights = gbm_interact$weights,
+#                  data = mhm)
+# 
+# 
+# 
+# mhq_interact <- svyglm(mhq ~ PA*age,
+#                       design = des_int,
+#                       family = gaussian())
+# 
+# 
+# summary(mhq_interact)
+# # Coefficients:
+# #              Estimate Std. Error t value Pr(>|t|)    
+# # (Intercept)    4.3888     3.7979   1.156 0.249093    
+# #   PA          25.6385     2.6505   9.673  < 2e-16 ***
+# #   age          14.9358     0.7656  19.509  < 2e-16 ***
+# #   PA:age       -2.0408     0.5796  -3.521 0.000522 *** 
+# confint(mhq_interact)
+# #                    2.5 %     97.5 %
+# #   (Intercept)   -3.095626 11.8731520
+# # PA              20.415341 30.8617081
+# # age             13.427074 16.4445412
+# # PA:age          -3.183107 -0.8985421
+# 
+# library(interactions)
+# interactions::interact_plot(mhq_interact,
+#                             pred = age, modx = PA)
+# # shown in pdf
+# 
 
 
 # CORE COGNITION INTERACTION ----------------------------------------------
-
-
-
-cog_interact <- svyglm(cog ~ PA*age,
-                       design = des_int,
-                       family = gaussian())
-
-
-summary(cog_interact)
-#   Estimate Std. Error t value Pr(>|t|)    
-#   (Intercept)  23.3558     2.9466   7.926 1.07e-13 ***
-#   PA           23.9955     2.3172  10.355  < 2e-16 ***
-#   age          13.7309     0.6392  21.482  < 2e-16 ***
-#   PA:age       -2.0901     0.5115  -4.086 6.12e-05 ***
-confint(cog_interact)
-#               2.5 %      97.5 %
-# (Intercept) 17.549114   29.162417
-# PA          19.429110   28.561956
-# age         12.471257   14.990448
-# PA:age      -3.098151   -1.082094
-
-
-
-# ADAPT RESILIENCE INTERACTION --------------------------------------------
-
-
-
-adaptresil_interact <- svyglm(adaptresil ~ PA*age,
-                       design = des_int,
-                       family = gaussian())
-
-summary(adaptresil_interact)
-#   Coefficients:
-#   Estimate Std. Error t value Pr(>|t|)    
-#   (Intercept)  37.7082     3.3839  11.143  < 2e-16 ***
-#   PA           21.6298     2.6617   8.126 3.03e-14 ***
-#   age          11.8276     0.7826  15.113  < 2e-16 ***
-#   PA:age       -1.1635     0.5790  -2.009   0.0457 * 
-confint(adaptresil_interact)
-#                  2.5 %      97.5 %
-# (Intercept) 31.039741    44.37673765
-# PA          16.384572    26.87508034
-# age         10.285315    13.36988039
-# PA:age      -2.304504   -0.02240528
-
-
-
-# MOOD AND OUTLOOK INTERACTION --------------------------------------------
-
-moodoutlook_interact <- svyglm(moodoutlook ~ PA*age,
-                              design = des_int,
-                              family = gaussian())
-
-summary(moodoutlook_interact)
-#                  Estimate Std. Error t value Pr(>|t|)    
-#    (Intercept)   6.0473     3.4598   1.748 0.081858 .  
-#   PA           23.3328     2.5859   9.023  < 2e-16 ***
-#   age          14.5869     0.6836  21.337  < 2e-16 ***
-#   PA:age       -2.0453     0.5881  -3.478 0.000607 ***
-confint(moodoutlook_interact)
-#                  2.5 %     97.5 %
-#   (Intercept) -0.7707047   12.8653970
-# PA            18.2368748   28.4288105
-# age           13.2396330   15.9341062
-# PA:age        -3.2042770   -0.8864145
-
-
-
-# DRIVE AND MOTIVATION INTERACTION ----------------------------------------
-
-drivemotiv_interact <- svyglm(drivemotiv ~ PA*age,
-                               design = des_int,
-                               family = gaussian())
-
-summary(drivemotiv_interact)
-#                Estimate Std. Error t value Pr(>|t|)    
-# (Intercept)    27.8938     3.4279   8.137 2.82e-14 ***
-#   PA           23.8058     2.8727   8.287 1.08e-14 ***
-#   age          13.0728     0.8971  14.573  < 2e-16 ***
-#   PA:age       -2.1501     0.6424  -3.347 0.000958 ***
-confint(drivemotiv_interact)
-#                  2.5 %    97.5 %
-#   (Intercept) 21.138597   34.648941
-# PA            18.144754   29.466836
-# age           11.305025   14.840630
-# PA:age        -3.415949   -0.884159
-
-
-
-# SOCIAL SELF INTERACTION -------------------------------------------------
-
-socialself_interact <- svyglm(socialself ~ PA*age,
-                              design = des_int,
-                              family = gaussian())
-
-summary(socialself_interact)
-#                 Estimate Std. Error t value Pr(>|t|)    
-#   (Intercept)  13.6556     4.3417   3.145  0.00189 ** 
-#   PA           16.8927     2.7404   6.164 3.28e-09 ***
-#   age          13.9698     0.8524  16.389  < 2e-16 ***
-#   PA:age       -1.0527     0.5798  -1.815  0.07079 . 
-confint(socialself_interact)
-#                  2.5 %        97.5 %
-#   (Intercept)  5.099643      22.21154227
-# PA             11.492352     22.29297674
-# age            12.290075     15.64962073
-# PA:age        -2.195314      0.08997785
-interactions::interact_plot(socialself_interact,
-                            pred = age, modx = PA)
-
-
-
-# MIND BODY INTERACTION lol -----------------------------------------------
-
-mindbody_interact <- svyglm(mindbody ~ PA*age,
-                         design = des_int,
-                         family = gaussian())
-
-summary(mindbody_interact)
-#   Estimate Std. Error t value Pr(>|t|)    
-# (Intercept)  26.8849     3.1432   8.553 1.93e-15 ***
-#   PA           28.3230     2.7484  10.305  < 2e-16 ***
-#   age           9.7027     0.8022  12.095  < 2e-16 ***
-#   PA:age       -2.2009     0.6556  -3.357 0.000927 ***
-confint(mindbody_interact)
-#                  2.5 %         97.5 %
-#   (Intercept) 20.690722    33.0790530
-#   PA          22.906916    33.7391577
-#   age          8.121834    11.2836220
-#   PA:age      -3.492921    -0.9088769
-
-
-
+# 
+# 
+# 
+# cog_interact <- svyglm(cog ~ PA*age,
+#                        design = des_int,
+#                        family = gaussian())
+# 
+# 
+# summary(cog_interact)
+# #   Estimate Std. Error t value Pr(>|t|)    
+# #   (Intercept)  23.3558     2.9466   7.926 1.07e-13 ***
+# #   PA           23.9955     2.3172  10.355  < 2e-16 ***
+# #   age          13.7309     0.6392  21.482  < 2e-16 ***
+# #   PA:age       -2.0901     0.5115  -4.086 6.12e-05 ***
+# confint(cog_interact)
+# #               2.5 %      97.5 %
+# # (Intercept) 17.549114   29.162417
+# # PA          19.429110   28.561956
+# # age         12.471257   14.990448
+# # PA:age      -3.098151   -1.082094
+# 
+# 
+# 
+# # ADAPT RESILIENCE INTERACTION --------------------------------------------
+# 
+# 
+# 
+# adaptresil_interact <- svyglm(adaptresil ~ PA*age,
+#                        design = des_int,
+#                        family = gaussian())
+# 
+# summary(adaptresil_interact)
+# #   Coefficients:
+# #   Estimate Std. Error t value Pr(>|t|)    
+# #   (Intercept)  37.7082     3.3839  11.143  < 2e-16 ***
+# #   PA           21.6298     2.6617   8.126 3.03e-14 ***
+# #   age          11.8276     0.7826  15.113  < 2e-16 ***
+# #   PA:age       -1.1635     0.5790  -2.009   0.0457 * 
+# confint(adaptresil_interact)
+# #                  2.5 %      97.5 %
+# # (Intercept) 31.039741    44.37673765
+# # PA          16.384572    26.87508034
+# # age         10.285315    13.36988039
+# # PA:age      -2.304504   -0.02240528
+# 
+# 
+# 
+# # MOOD AND OUTLOOK INTERACTION --------------------------------------------
+# 
+# moodoutlook_interact <- svyglm(moodoutlook ~ PA*age,
+#                               design = des_int,
+#                               family = gaussian())
+# 
+# summary(moodoutlook_interact)
+# #                  Estimate Std. Error t value Pr(>|t|)    
+# #    (Intercept)   6.0473     3.4598   1.748 0.081858 .  
+# #   PA           23.3328     2.5859   9.023  < 2e-16 ***
+# #   age          14.5869     0.6836  21.337  < 2e-16 ***
+# #   PA:age       -2.0453     0.5881  -3.478 0.000607 ***
+# confint(moodoutlook_interact)
+# #                  2.5 %     97.5 %
+# #   (Intercept) -0.7707047   12.8653970
+# # PA            18.2368748   28.4288105
+# # age           13.2396330   15.9341062
+# # PA:age        -3.2042770   -0.8864145
+# 
+# 
+# 
+# # DRIVE AND MOTIVATION INTERACTION ----------------------------------------
+# 
+# drivemotiv_interact <- svyglm(drivemotiv ~ PA*age,
+#                                design = des_int,
+#                                family = gaussian())
+# 
+# summary(drivemotiv_interact)
+# #                Estimate Std. Error t value Pr(>|t|)    
+# # (Intercept)    27.8938     3.4279   8.137 2.82e-14 ***
+# #   PA           23.8058     2.8727   8.287 1.08e-14 ***
+# #   age          13.0728     0.8971  14.573  < 2e-16 ***
+# #   PA:age       -2.1501     0.6424  -3.347 0.000958 ***
+# confint(drivemotiv_interact)
+# #                  2.5 %    97.5 %
+# #   (Intercept) 21.138597   34.648941
+# # PA            18.144754   29.466836
+# # age           11.305025   14.840630
+# # PA:age        -3.415949   -0.884159
+# 
+# 
+# 
+# # SOCIAL SELF INTERACTION -------------------------------------------------
+# 
+# socialself_interact <- svyglm(socialself ~ PA*age,
+#                               design = des_int,
+#                               family = gaussian())
+# 
+# summary(socialself_interact)
+# #                 Estimate Std. Error t value Pr(>|t|)    
+# #   (Intercept)  13.6556     4.3417   3.145  0.00189 ** 
+# #   PA           16.8927     2.7404   6.164 3.28e-09 ***
+# #   age          13.9698     0.8524  16.389  < 2e-16 ***
+# #   PA:age       -1.0527     0.5798  -1.815  0.07079 . 
+# confint(socialself_interact)
+# #                  2.5 %        97.5 %
+# #   (Intercept)  5.099643      22.21154227
+# # PA             11.492352     22.29297674
+# # age            12.290075     15.64962073
+# # PA:age        -2.195314      0.08997785
+# interactions::interact_plot(socialself_interact,
+#                             pred = age, modx = PA)
+# 
+# 
+# 
+# # MIND BODY INTERACTION lol -----------------------------------------------
+# 
+# mindbody_interact <- svyglm(mindbody ~ PA*age,
+#                          design = des_int,
+#                          family = gaussian())
+# 
+# summary(mindbody_interact)
+# #   Estimate Std. Error t value Pr(>|t|)    
+# # (Intercept)  26.8849     3.1432   8.553 1.93e-15 ***
+# #   PA           28.3230     2.7484  10.305  < 2e-16 ***
+# #   age           9.7027     0.8022  12.095  < 2e-16 ***
+# #   PA:age       -2.2009     0.6556  -3.357 0.000927 ***
+# confint(mindbody_interact)
+# #                  2.5 %         97.5 %
+# #   (Intercept) 20.690722    33.0790530
+# #   PA          22.906916    33.7391577
+# #   age          8.121834    11.2836220
+# #   PA:age      -3.492921    -0.9088769
+# 
+# 
+# 
 
 # SENSITIVITY ANALYSIS: MI + CBPS -----------------------------------------
 
